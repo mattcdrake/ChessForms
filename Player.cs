9b293aa7ec1eb549962b7e7dc2872b74a3fcff0d
mattcdrake <drake.mattc@gmail.com>
2018-01-17 21:27:49 -0500

    initial

---
+++ Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public enum Color {White, Black};
+
+public class Player
+{
+	Color color;
+	int score;
+
+	public Player(Color color)
+	{
+        this.color = color;
+		this.score = 0;
+	}
+}
